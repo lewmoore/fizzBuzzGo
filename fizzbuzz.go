6abd755b10034fcc5434865ed3a7922941aa3153
Lewis Moore <lewis.moore2707@gmail.com>
2019-03-28 15:34:02 +0000

    Add case for 5 == Buzz

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -4,6 +4,9 @@ func fizzBuzz(number int) string {
 	if number == 3 {
 		return "Fizz"
 	}
+	if number == 5 {
+		return "Buzz"
+	}
 	return ""
 }
 
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -3,12 +3,17 @@ package main
 import "testing"
 
 func TestFizzBuzzEqualsFizzWhenMultipleOfThree(t *testing.T) {
-	// got := fizzBuzz(3)
-	// if got != "Fizz" {
-	// 	t.Errorf("fizzBuzz(3) returns %d; want Fizz", got)
-	// }
 	result := fizzBuzz(3)
 	expect := "Fizz"
+
+	if result != expect {
+		t.Fail()
+	}
+}
+
+func TestFizzBuzzEqualsBuzzWhenMultipleOfFive(t *testing.T) {
+	result := fizzBuzz(5)
+	expect := "Buzz"
 
 	if result != expect {
 		t.Fail()
